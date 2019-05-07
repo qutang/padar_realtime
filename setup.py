eab0059a9c256b3d0ec59c21ca0a982218d63c6a
qutang <tqshelly@gmail.com>
2019-05-07 18:06:17 -0400

    v1.4.2
    Update dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='padar_realtime',
-    version='1.4.1',
+    version='1.4.2',
     packages=find_packages(),
     include_package_data=True,
     description=
     'Real-time engine used to process and visualize accelerometer data for padar package',
     long_description=open('README.md').read(),
-    install_requires=["websockets", "flask", "arrow", "pymetawear", "pandas"],
+    install_requires=[
+        "websockets", "flask", "arrow", "pymetawear", "pandas", 'metawear',
+        'warble'
+    ],
 )
